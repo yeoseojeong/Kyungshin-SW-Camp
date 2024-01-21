5b2639bbde2470a6cb24d949dd53d8c5dbde10c7
Deodeong <121150215+yeoseojeong@users.noreply.github.com>
2024-01-21 16:27:26 +0900

    Create asw.c

---
+++ Arduino/Race condition_resource/asw.c
@@ -0,0 +1,39 @@
+#include "bsw.h"
+
+volatile unsigned long shared = 0;
+
+ISR2(TimerISR)
+{
+   IncrementCounter(counter1);
+}
+
+TASK(Task1){
+      unsigned long i;
+   printfSerial("Task1 Begins...\n");
+   for(i = 0; i<1000000; i++){
+	   GetResource(S1);
+      shared++;
+      ReleaseResource(S1);
+   }
+   printfSerial("Added 1000000 to shared\n");
+   printfSerial("shared = %4lu\n", shared);
+   printfSerial("Task1 finishes...");
+   TerminateTask();
+}
+
+TASK(Task2){
+	static unsigned long i=0;
+	if(i<1000)
+	{
+		GetResource(S1);
+		shared++;
+		ReleaseResource(S1);
+	}
+	else if(i==1000)
+	{
+		printfSerial("Added 1000 to shared\n");
+	}
+   i++;
+   TerminateTask();
+}
+
